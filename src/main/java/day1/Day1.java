04e575db5617e5f93da57a2dbe8de75e3c1df9dd
Marcin Michalak <plateauu@o2.pl>
2018-12-02 11:24:04 +0100

    Added junit

--- src/main/java/day1/Day1.java
+++ src/main/java/day1/Day1.java
@@ -9,34 +9,43 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
-public class App {
+public class Day1 {
 
-    private static final Logger log = LoggerFactory.getLogger(App.class.getSimpleName());
-    private static final Map<Long, Integer> map = new HashMap<>();
-    private static final String INPUT = "src/main/resources/day1/input.txt";
+    private static final Logger log = LoggerFactory.getLogger(Day1.class.getSimpleName());
+    private final Map<Long, Integer> map = new HashMap<>();
+    private final String INPUT = "src/main/resources/day1/input.txt";
 
-    private static long result = 0;
+    private long result = 0;
 
     public static void main(String[] args) throws IOException {
+        Day1 day1 = new Day1();
+        day1.resolvePart1();
+        day1.resolvePart2();
+    }
+
+    private long resolvePart1() throws IOException {
         log.info(String.format("PART 1"));
         long sum = Files.lines(Paths.get(INPUT))
                 .mapToLong(Long::valueOf)
                 .sum();
-
         log.info(String.format("Finished frequency: %d", sum));
+        return sum;
+    }
 
+    private long resolvePart2() throws IOException {
         log.info(String.format("PART 2"));
+        Long duplicated;
         while (true) {
-            proceed();
-            if (getResult() != null) {
+            readFrequencies();
+            if ((duplicated = getDuplicatedFrequenecies()) != null) {
                 break;
             }
         }
-
-        log.info(String.format("Frequency first reached twice is: %d", getResult()));
+        log.info(String.format("Frequency first reached twice is: %d", duplicated));
+        return duplicated;
     }
 
-    private static void proceed() throws IOException {
+    private void readFrequencies() throws IOException {
         FileInputStream is = new FileInputStream(new File(INPUT));
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         String line;
@@ -50,12 +59,11 @@ public class App {
         }
     }
 
-    private static Long getResult() {
+    private Long getDuplicatedFrequenecies() {
         return map.entrySet().stream()
                 .filter(e -> e.getValue() > 1)
                 .map(Map.Entry::getKey)
                 .findFirst()
                 .orElse(null);
     }
-
 }
